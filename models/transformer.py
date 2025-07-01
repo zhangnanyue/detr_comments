30f63b98c73482c792f663098b1ddbed6f245f9c
zhangnanyue <zhangnanyue@neolix.cn>
2025-07-01 16:35:23 +0800

    vault backup: 2025-07-01 16:35:23

--- models/transformer.py
+++ models/transformer.py
@@ -151,8 +151,6 @@ class TransformerEncoderLayer(nn.Module):
                      src_mask: Optional[Tensor] = None,
                      src_key_padding_mask: Optional[Tensor] = None,
                      pos: Optional[Tensor] = None):
-        # DETR 不在最开始简单地将特征和位置编码相加，而是在每个注意力层内部动态地注入位置信息。
-        # 将空间位置编码 pos 加到 src 上，生成 Q 和 K
         q = k = self.with_pos_embed(src, pos)
         src2 = self.self_attn(q, k, value=src, attn_mask=src_mask,
                               key_padding_mask=src_key_padding_mask)[0]
@@ -168,6 +166,8 @@ class TransformerEncoderLayer(nn.Module):
                     src_key_padding_mask: Optional[Tensor] = None,
                     pos: Optional[Tensor] = None):
         src2 = self.norm1(src)
+        # DETR 不在最开始简单地将特征和位置编码相加，而是在每个注意力层内部动态地注入位置信息。
+        # 将空间位置编码 pos 加到 src 上，生成 Q 和 K
         q = k = self.with_pos_embed(src2, pos)
         src2 = self.self_attn(q, k, value=src2, attn_mask=src_mask,
                               key_padding_mask=src_key_padding_mask)[0]
